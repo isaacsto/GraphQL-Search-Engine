aec8b0bf9434af1eeeb3b857ddd3f9b1f08d3c98
Isaac Stofko <isaacstofko@gmail.com>
2023-06-20 13:43:11 -0400

    typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,44 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+type User {
+    _id: ID
+    username: String
+    email: String
+    passwords: String
+    savedBooks: [bookSchema]
+}
+
+type bookSchema {
+    _id: ID
+    authors: String 
+    description: String 
+    bookId: String 
+    image: String 
+    link: String
+    title: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    users: [User]
+    user(username: String!) : User
+    books(username: String) : [bookSchema]
+    book(bookId: ID!) : bookSchema
+    me: User
+}
+
+type Mutation {
+    addUser(username: String! email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(bookId: String!): bookSchema
+    removeBook(bookId: ID!): bookSchema
+}
+`
+;
+
+module.exports = typeDefs;
